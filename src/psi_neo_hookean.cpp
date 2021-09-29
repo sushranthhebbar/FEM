734ec424aab4fbfb84e9d7732da109aa5c43440a
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-09-29 21:36:00 +0530

    Computed neo hookean energy

--- src/psi_neo_hookean.cpp
+++ src/psi_neo_hookean.cpp
@@ -5,6 +5,8 @@ void psi_neo_hookean(double &psi,
                      Eigen::Ref<const Eigen::Matrix3d> F,
                      double C, double D) {
 
-    
+    double J = F.determinant();
+    Eigen::Matrix3d RCGD = F.transpose() * F;
+    psi = C * (pow(J, -2/3.0) * RCGD.trace() - 3) + D * (J - 1) * (J - 1);
 
 }
