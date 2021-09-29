34e9a11c8cb12d3883c72ea8405587594e3ffc35
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-09-29 19:43:05 +0530

    Added the shape basis function phi

--- test.cpp
+++ test.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int main()
 {
-    Eigen::RowVectorXd vec1(3);
+    Eigen::VectorXd vec1(3);
     vec1 << 1, 2, 3;
 
-    Eigen::RowVectorXd vec2(3);
+    Eigen::VectorXd vec2(3);
     vec2 << 4, 5, 6;
 
-    Eigen::RowVectorXd vec3(3);
+    Eigen::VectorXd vec3(3);
     vec3 << 7, 8, 9;
 
 
     Eigen::MatrixXd m(3, 3);
     m << vec1, vec2, vec3;
 
-    cout<<m.transpose()<<endl;
+    cout<<m<<endl;
 
     return 0;
 }
