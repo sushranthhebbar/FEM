9d95741bfb878bae17fb5d5e303f7dcc04015a3c
Sushranth Hebbar <sushranthhebbar@gmail.com>
2021-09-30 20:35:13 +0530

    Done with assemble forces

--- src/assemble_forces.cpp
+++ src/assemble_forces.cpp
@@ -1,11 +1,26 @@
 #include <assemble_forces.h>
 #include <iostream>
 
-void assemble_forces(Eigen::VectorXd &f, Eigen::Ref<const Eigen::VectorXd> q, Eigen::Ref<const Eigen::MatrixXd> qdot, 
+void assemble_forces(Eigen::VectorXd &f, Eigen::Ref<const Eigen::VectorXd> q, Eigen::Ref<const Eigen::VectorXd> qdot, 
                      Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::MatrixXi> T, Eigen::Ref<const Eigen::VectorXd> v0,
                      double C, double D) { 
         
-        
-        
+        f.resize(3 * V.rows());
+        f.setZero();
+        for(int i = 0; i < T.rows(); i++)
+        {
+            Eigen::RowVectorXi element = T.row(i);
+            Eigen::Vector12d fi;
+            dV_linear_tetrahedron_dq(fi, q, V, element, v0(i), C, D);
+            fi = -1 * fi;
+            for(int i = 0; i < 4; i++)
+            {
+                int index = element(i);
+                for(int j = 0; j < 3; j++)
+                {
+                    f(3*index + j) = f(3*index + j) + fi(3*i + j);
+                }
+            }
+        }
 
     };
